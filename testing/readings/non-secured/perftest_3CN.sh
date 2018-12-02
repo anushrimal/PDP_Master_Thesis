2ba6bf160428aff3b93e5a284ec95ae619a759d2
Ubuntu <ubuntu@hoernli-8.maas>
2018-12-02 21:57:29 +0000

    Readings and lua script for wrk2

---
+++ testing/readings/non-secured/perftest_3CN.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for file in /home/ubuntu/test_files/*
+do
+        fileN=${file##*/}
+        cmd="time curl -H Transfer-Encoding:chunked -F FileName="$fileN" -F FileData=@"$file" http://172.28.1.180:6666/resources/"
+        echo $cmd
+        output=`($cmd 2>&1)`
+        echo $output
+        ret=`echo $output | sed -n 's/.*system//;s/elapsed.*//p'`
+        echo "\n======\nRETURN : " + $ret + "\n=======\n"
+        echo $fileN,$ret >> sendFiles_3CN.csv
+done
+
+for file in /home/ubuntu/test_files/*
+do
+        fileN=${file##*/}
+        cmd="time curl http://172.28.1.180:6666/resources/"$fileN" -o first_rep.txt"
+        echo $cmd
+        output=`($cmd 2>&1)`
+        ret=`echo $output | sed -n 's/.*system//;s/elapsed.*//p'`
+        echo "\n======\nRETURN : " + $ret + "\n=======\n"
+        echo ${fileN##*/},$ret >> getFiles_3CN.csv
+done
+
---
+++ testing/readings/secured/perftest_2CN.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+for file in /home/ubuntu/test_files/*
+do
+        fileN=${file##*/}
+        cmd="time curl -H Transfer-Encoding:chunked -F FileName="$fileN" -F Secured="true" -F FileData=@"$file" http://172.28.1.180:6666/resources/"
+        echo $cmd
+        output=`($cmd 2>&1)`
+        echo $output
+        ret=`echo $output | sed -n 's/.*system//;s/elapsed.*//p'`
+        echo "\n======\nRETURN : " + $ret + "\n=======\n"
+        echo $fileN,$ret >> sendFiles_2CN.csv
+done
+
+for file in /home/ubuntu/test_files/*
+do
+        fileN=${file##*/}
+        cmd="time curl http://172.28.1.180:6666/resources/"$fileN" -o first_rep.txt"
+        echo $cmd
+        output=`($cmd 2>&1)`
+        ret=`echo $output | sed -n 's/.*system//;s/elapsed.*//p'`
+        echo "\n======\nRETURN : " + $ret + "\n=======\n"
+        echo ${fileN##*/},$ret >> getFiles_2CN.csv
+done
+
+for file in /home/ubuntu/test_files/*
+do
+        fileN=${file##*/}
+        cmd="time curl -XCHALLENGE http://172.28.1.180:6666/challenge/"$fileN""
+        echo $cmd
+        output=`($cmd 2>&1)`
+        ret=`echo $output | sed -n 's/.*system//;s/elapsed.*//p'`
+        echo "\n======\nRETURN : " + $ret + "\n=======\n"
+        echo ${fileN##*/},$ret >> challFiles_2CN.csv
+done
+
+
